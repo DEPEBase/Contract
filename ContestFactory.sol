72a1a61c5b19e2aed16e38023d8eb5ce0b73dc4f
Dracklyn <83720335+Dracklyn@users.noreply.github.com>
2025-10-08 15:58:50 +0200

    Name

--- ContestFactory.sol
+++ ContestFactory.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
-import "./ContestInstanceV2.sol";
+import "./ContestInstance.sol";
 
 /**
  * @title ContestFactory
